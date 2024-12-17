c42bf5c394ef7c9468f8588841b3f17f74d5460e
osmanrkhan <osmanrkhan@gmail.com>
2024-12-17 02:33:09 -0500

    let's try that again

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // Ensures relative paths for assets
+  base: '/poisson-party-predictor/', // Ensures relative paths for assets
 });
