63f8833d54e0f3aa9282cc108d776ce1051eb981
osmanrkhan <osmanrkhan@gmail.com>
2024-12-17 01:57:21 -0500

    change to relative paths

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/./', // Ensures relative paths for assets
+  base: '', // Ensures relative paths for assets
 });
